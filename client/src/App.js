ffd182fd8f68105f8d3238e590719c30d23110d8
bardromi <s2dtsxSy>
2020-06-11 21:49:45 +0300

    Added Carousel

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,37 @@
 import React, {useState, useEffect} from 'react';
 import Container from '@material-ui/core/Container';
 import axios from 'axios';
-import logo from './geffen.jpeg';
 import './App.css';
 import WishList from './components/WishList';
+import Carousel from 'react-material-ui-carousel'
+import {Paper} from '@material-ui/core'
+import {makeStyles} from "@material-ui/core/styles";
 
+
+const useStyles = makeStyles({
+    img: {
+        maxWidth: '100%',
+    },
+    container: {
+        paddingTop: '10px'
+    }
+});
+
+function Item({imageName}) {
+    const classes = useStyles();
+
+    return (
+        <Paper>
+            <img alt={classes.img} className={classes.img} src={require(`./images/${imageName}.png`)}/>
+        </Paper>
+    )
+}
 
 const App = () => {
+    const items = ['carousel1', 'carousel2'];
+    const classes = useStyles();
+
+
     const [isLoading, setIsLoading] = useState(false);
     const [data, setData] = useState([]);
 
@@ -25,22 +50,27 @@ const App = () => {
     }, []);
 
     return (
-        <div id="animated-gif-container" className="App">
-            <Container fixed>
+        <Container fixed className={classes.container}>
 
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                </header>
-                <div className="Main">
-                    {isLoading ? (
-                        <div>Loading ...</div>
-                    ) : (
-                        <WishList wishes={data}/>
-                    )}
+            {/*<header className="App-header">*/}
+            {/*    <img src={logo} className="App-logo" alt="logo"/>*/}
+            {/*</header>*/}
+            <Carousel
+                autoPlay={true}
+            >
+                {
+                    items.map(item => <Item imageName={item}/>)
+                }
+            </Carousel>
+            <div className="Main">
+                {isLoading ? (
+                    <div>Loading ...</div>
+                ) : (
+                    <WishList wishes={data}/>
+                )}
 
-                </div>
-            </Container>
-        </div>
+            </div>
+        </Container>
     );
 }
 
